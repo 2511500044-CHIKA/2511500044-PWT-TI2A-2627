beff077403d4f0993fe6a84d994b86ebade639bc
chika zayin salsabila <2511500044@mahasiswa.atmaluhur.ac.id>
2026-06-15 04:47:45 -0600

    update

--- login.php
+++ login.php
@@ -32,7 +32,7 @@ session_start();
 
         <form action="login.php" method="post">
           <div class="input-group mb-3">
-            <input type="text" name="usersname" class="form-control" placeholder="usersname">
+            <input type="text" name="username" class="form-control" placeholder="username">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -77,7 +77,7 @@ $username = $_POST['username'] ?? null;
 $password = $_POST['password'] ?? null;
 
 if ($username == true) {
-  $userquary = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username'"));
+  $userquary = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM user WHERE username='$username'"));
 
   if ($userquary) {
 
