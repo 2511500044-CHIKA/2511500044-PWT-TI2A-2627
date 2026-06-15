6f8e8c80553ff3d5d9c3e0facb67f9669cf2fb76
chika zayin salsabila <2511500044@mahasiswa.atmaluhur.ac.id>
2026-06-15 04:45:48 -0600

    update file index.php

--- Index.php
+++ Index.php
@@ -2,9 +2,9 @@
 ob_start();
 session_start();
 require_once("config/koneksi.php");
-if (isset($_SESSION['usersname'])) {
-  $usersname = $_SESSION['usersname'];
-  $query = mysqli_query($koneksi, "SELECT * FROM users WHERE usersname='$usersname'");
+if (isset($_SESSION['username'])) {
+  $username = $_SESSION['username'];
+  $query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username'");
   $result = mysqli_fetch_array($query);
   $role = $result['role'];
 ?>
